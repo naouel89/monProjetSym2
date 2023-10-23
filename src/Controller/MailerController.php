030ae3faa17d8b37100893a4f5144afddc7289d5
naouel89 <jessus.n@gmail.COM>
2023-10-23 11:00:29 +0200

    image

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use SebastianBergmann\Template\Template;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\File;
+
+
+class MailerController extends AbstractController
+{
+    #[Route('/email', name: 'app_mailer')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        try {
+            $email = (new TemplatedEmail())
+                ->from('jessus.n@gmail.com')
+                ->to('test@example.com')
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->htmlTemplate('mailer/index.html.twig'); 
+                // $email = (new TemplateEmail())
+                // // ...
+            
+                // // utiliser la syntaxe 'cid:' + "nom de l'image intégrée " pour référencer l'image
+                // ->html('<img src="cid:logo"> ... <img src="cid:footer-signature"> ...')
+                
+            
+                // // utiliser la même syntaxe pour les images intégrées en tant que background
+                // ->html('... <div background="cid:footer-signature"> ... </div> ...')
+            ;
+            $mailer->send($email);
+
+            return new Response('Email sent successfully');
+        } catch (\Exception $e) {
+            return new Response('Error sending email: ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ src/Controller/testaffiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArtistRepository;
+use App\Repository\DiscRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+class testaffiController extends AbstractController
+{
+    
+
+    private $artistRepo;
+    private $discRepo;
+    private $em;
+
+    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo, EntityManagerInterface $em)
+    {
+        $this->artistRepo = $artistRepo;
+        $this->discRepo = $discRepo;
+        $this->em = $em;
+
+    }
+    #[Route('/testaffi', name: 'app_testaffi')]
+    public function index(): Response
+    {
+       //on appelle le repository pour accéder à la fonction
+        $artistes = $this->artistRepo->getSomeArtists("Serge Gainsbourg");
+
+        //on teste le contenu de la variable $artistes : dd() veut dire Dump and Die
+        dd($artistes); 
+
+     // ...    
+
+    }
+
+    
+
+}
